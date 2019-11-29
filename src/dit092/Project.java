eed11df650d35641bfcd3af5b2a8b5fd28679922
hashmati <hashmati@student.chalmers.se>
2019-11-29 16:02:35 +0100

    Adding Project class

---
+++ src/dit092/Project.java
@@ -0,0 +1,16 @@
+package dit092;
+
+import java.util.ArrayList;
+
+public class Project {
+
+	private Team team; 
+	private ArrayList <Task> task;
+	
+	public Project (Team team, ArrayList <Task> task) {
+		this.team = team;
+		this.task = task;
+	}
+	
+	
+}
