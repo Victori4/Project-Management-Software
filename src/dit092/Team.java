274c9740c3dd1bdee2f1d464e32648bdfb87d6a2
vuvi <vuvi@student.chalmers.se>
2019-11-30 11:46:34 +0100

    Replaced classic loops with enhanced for loops, kept old method commented out for now

--- src/dit092/Team.java
+++ src/dit092/Team.java
@@ -13,7 +13,17 @@ package dit092;
 		}
 		
 		
-		//total salary of all members on this team using classic for loop
+
+		//total salary of all members on this team using enhanced for loop
+		public double totalSalary (ArrayList <TeamMember> members) {
+		double sum = 0;
+		for (TeamMember member : members) {
+		sum = sum + member.calculateSalary(member.getTimeSpent());
+		}
+		return sum;
+		}
+		
+		/*total salary of all members on this team using classic for loop in case enhanced does not work
 		public double totalSalary (ArrayList <TeamMember> members) {
 			double sum = 0;
 			for (int i =0; i < members.size(); i++) {
@@ -21,16 +31,26 @@ package dit092;
 			}
 			return sum;
 		}
+		*/
 		
-		//Total time spent by whole team on project using classic for loop
+//total time spent of all members using enhanced for loop
+		public double totalTimeSpent() {
+			double totalTime = 0;
+			for (TeamMember member : members) {
+				totalTime = totalTime + member.getTimeSpent();
+			}
+			return totalTime;
+		}
+			
+		
+		/*Total time spent by whole team on project using classic for loop in case enhanced does not work
 		public double totalTimeSpent() {
 			double totalTime = 0;
 			for (int i =0; i < members.size(); i++) {
 				totalTime = totalTime + members.get(i).getTimeSpent();
 			}
 			return totalTime;
-		}
-			
+		}*/
 
 		public int getId() {
 			return id;
