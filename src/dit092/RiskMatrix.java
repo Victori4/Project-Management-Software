fa2c6cf04783783d6163b91a1f25cbe1f85cf72f
hashmati <hashmati@student.chalmers.se>
2019-12-05 14:55:36 +0100

    Finishing the start of risk matrix class, added getters and setters

--- src/dit092/RiskMatrix.java
+++ src/dit092/RiskMatrix.java
@@ -4,12 +4,14 @@ public class RiskMatrix {
 
 	private double impact;
 	private double probability;
-	public double risk;
+	private double risk;
+	private String riskName;
 	
-	public RiskMatrix (double impact, double probability, double risk) {
+	public RiskMatrix (double impact, double probability, double risk, String riskName) {
 		this.impact = impact;
 		this.probability = probability;
 		this.risk = risk;
+		this.riskName = riskName;
 	}
 	
 	public double calculateRisk() {
@@ -17,4 +19,19 @@ public class RiskMatrix {
 		return risk;
 	}
 	
+	public double getRisk() {
+		return risk;
+	}
+	
+	public String getRiskName() {
+		return riskName;
+	}
+	
+	public void setRisk(double risk) {
+		this.risk = risk;
+	}
+
+	public void setRiskName(String riskName) {
+		this.riskName = riskName;
+	}
 }
