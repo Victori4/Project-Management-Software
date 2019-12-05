a3b431b1ae7bbd99031345158e125eb3d100f947
nordje <nordje@student.chalmers.se>
2019-12-05 23:11:01 +0100

    First mvc draft

--- src/dit092/ProjectManagement.java
+++ src/dit092/ProjectManagement.java
@@ -1,20 +1,73 @@
 package dit092;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ProjectManagement {
 
     private UserInterface view;
+    private DataAccessLayer data;
+    private Project project;
 
-    public ProjectManagement(UserInterface view) {
-        this.view = view;
+    //Menu options
+    private static final int EXIT = 0;
+    private static final int START = 1;
+    private static final int EV = 2;
+    private static final int SV = 3;
+    private static final int CV = 4;
+    private static final int PV = 5;
+    private static final int AC = 6;
+    private static final int BAC = 7;
+    private static final int CPI = 8;
+    private static final int SPI = 9;
+    private static final int RISK_MATRIX = 10;
+    private static final int PROJECT_SCHEDULE = 11;
+    private static final int SEARCH_TEAM_MEMBER_HOURS_BY_ID = 12;
+    private static final int SEARCH_TEAM_MEMBER_ACTIVITIES_BY_ID = 13;
+
+    public ProjectManagement() {
+        this.view = new UserInterface();
+        this.data = new DataAccessLayer(); //Send file path in here?
+
+        ArrayList<Task> testTasks = new ArrayList<Task>();
+        ArrayList<TeamMember> testTeamMembers = new ArrayList<TeamMember>();
+        Task task1 = new Task(1, "One", 1, 1, 1,
+                2, 2, 10, 10, testTeamMembers);
+        Task task2 = new Task(2, "Two",1, 2, 2,
+                2,2, 5, 5, testTeamMembers);
+        Task task3 = new Task(3, "Three",1, 3, 3,
+                4, 4, 5, 5, testTeamMembers);
+        testTasks.add(task1);
+        testTasks.add(task2);
+        testTasks.add(task3);
+        this.project = new Project(testTasks, new Team(1, testTeamMembers));
+        //this.project = data.loadProject(); ?
     }
 
     public void run() {
-        //Main menu
+        int userInput = START;
+
+        do {
+            switch (userInput = view.showStartMenu()) {
+                case START:
+                    userInput = view.showStartMenu();
+                    break;
+                case EV:
+                    //Obviously get the weeks some other way, perhaps getEarnedValue() which could return a string?
+                    userInput = view.showEarnedValue(project.calculateEarnedValue(1,2));
+                    break;
+                case EXIT:
+                    userInput = 0;
+                    break;
+                default:
+                    view.showDefault();
+                    break;
+            }
+        } while (userInput != EXIT);
     }
 
     public static void main(String[] args) {
-        UserInterface view = new UserInterface();
-        ProjectManagement controller = new ProjectManagement(view);
+        ProjectManagement controller = new ProjectManagement();
 
         controller.run();
     }
--- src/dit092/UserInterface.java
+++ src/dit092/UserInterface.java
@@ -1,5 +1,28 @@
 package dit092;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    private Scanner scanner;
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int showStartMenu() {
+        System.out.println("MENU\nPlease press 2 to see EV for week 1-2 or 0 to exit");
+        return scanner.nextInt();
+    }
+
+    public int showEarnedValue(double ev) {
+        System.out.print(ev);
+
+        return scanner.nextInt();
+    }
+
+    public void showDefault() {
+        System.out.println("Please try again!");
+    }
 
 }
