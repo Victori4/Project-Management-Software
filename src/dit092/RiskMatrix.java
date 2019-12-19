6d6ee585c45b388ab926d13b3761d1076abe29c6
carlars <carlars@student.chalmers.se>
2019-12-19 15:24:39 +0100

    Reformatted and commented Risk and Risk Matrix classes

--- src/dit092/RiskMatrix.java
+++ src/dit092/RiskMatrix.java
@@ -2,25 +2,51 @@ package dit092;
 
 import java.util.ArrayList;
 
+/**
+ * A collection of risks represented as a risk matrix
+ */
 public class RiskMatrix {
 	ArrayList<Risk> risks;
 	
+	/**
+	 * Creates an empty, default risk matrix
+	 */
 	public RiskMatrix() {
 		risks = new ArrayList<Risk>();
 	}
-	
+
+	/**
+	 * Creates a risk matrix with one risk
+	 * @param risk
+	 */
 	public RiskMatrix(Risk risk) {
 		risks = new ArrayList<Risk>();
-		risks.add(risk);
+		addRisk(risk);
 	}
-	
+
+	/**
+	 * Creates a risk matrix with a list of risks
+	 * @param risks
+	 */
 	public RiskMatrix(ArrayList<Risk> risks) {
 		risks = new ArrayList<Risk>();
-		risks.addAll(risks);
+		addRisks(risks);
 	}
 	
+	/**
+	 * Adds a risk to the risk matrix
+	 * @param risk the risk to be added
+	 */
 	public void addRisk(Risk risk) {
 		risks.add(risk);
+	}
+	
+	/**
+	 * Adds a list of risks to the risk matrix
+	 * @param risks the list of risks to be added
+	 */
+	public void addRisks(ArrayList<Risk> risks) {
+		this.risks.addAll(risks);
 	}
 
 	//TODO RiskMatrix toString()
