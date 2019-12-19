524be7ffc6d5abd09a5444e508c5f4eb5b97be0c
vuvi <vuvi@student.chalmers.se>
2019-12-19 15:12:01 +0100

    json stuff

--- src/dit092/DataAccessLayer.java
+++ src/dit092/DataAccessLayer.java
@@ -1,5 +1,36 @@
 package dit092;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.parser.*;
+
 
 public class DataAccessLayer {
+private final String filepath = "././TEST.json";
+	
+	public DataAccessLayer () {
+		
+	}
+	
+	
+	public Project loadProject() {
+		JSONParser jsonParser = new JSONParser();
+		try (FileReader reader = new FileReader(filepath)){
+			Object jsonObject = jsonParser.parse(reader);
+			Project project = (Project) jsonObject;
+			return project;
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+		e.printStackTrace();	
+		} catch(ParseException e) {
+		e.printStackTrace();	
+		}  
+		return null;
+	
+	}
 
 }
