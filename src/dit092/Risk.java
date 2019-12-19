2be3d80c28a949c9d556c714debf5d7ffe9ec4fa
carlars <carlars@student.chalmers.se>
2019-12-19 11:22:54 +0100

    Add Risk.java

---
+++ src/dit092/Risk.java
@@ -0,0 +1,38 @@
+package dit092;
+
+public class Risk {
+	
+	private String riskName;
+	private double impact;
+	private double probability;
+	
+	public Risk(String riskName, double impact, double probability) {
+		this.riskName = riskName;
+		this.impact = impact;
+		this.probability = probability;
+	}
+
+	public String getRiskName() {
+		return riskName;
+	}
+
+	public void setRiskName(String riskName) {
+		this.riskName = riskName;
+	}
+
+	public double getImpact() {
+		return impact;
+	}
+
+	public void setImpact(double impact) {
+		this.impact = impact;
+	}
+
+	public double getProbability() {
+		return probability;
+	}
+
+	public void setProbability(double probability) {
+		this.probability = probability;
+	}
+}
